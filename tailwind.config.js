ade16aa2354fb7595c642ec2de0345e564127b4a
Goktug YILDIZ <goktug.7tepe@gmail.com>
2023-11-08 18:20:06 -0800

    feature/27/ add animation to like and repost button

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
       flexGrow: {
         4: "0.4",
       },
+
       colors: {
         border: "var(--border)",
         input: "var(--input)",
@@ -77,10 +78,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        pulse: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.25)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        pulse: "pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
     },
   },
