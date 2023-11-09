fac967ba93c77c954d5584acd718596091fe3d64
kanta1207 <kan0712livepool@gmail.com>
2023-11-09 11:27:41 -0800

    fix : green color naming and color for dark theme

--- src/components/ui/RepostButton.tsx
+++ src/components/ui/RepostButton.tsx
@@ -1,5 +1,5 @@
-import React, { useRef, useEffect } from "react";
-import gsap from "gsap";
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
 
 type RepostProps = {
   isReposted: boolean;
@@ -15,12 +15,12 @@ const Repost2: React.FC<RepostProps> = ({ isReposted, onClick }) => {
       gsap.to(repostRef.current, {
         scale: 1.25,
         duration: 0.5,
-        ease: "elastic.out(1, 0.5)",
+        ease: 'elastic.out(1, 0.5)',
         repeat: 1,
       });
     } else {
       // Reverse the animation when cliked to un-repost
-      gsap.to(repostRef.current, { scale: 1, duration: 0.2, ease: "none" });
+      gsap.to(repostRef.current, { scale: 1, duration: 0.2, ease: 'none' });
     }
   }, [isReposted]);
 
@@ -28,15 +28,13 @@ const Repost2: React.FC<RepostProps> = ({ isReposted, onClick }) => {
     <svg
       onClick={onClick}
       ref={repostRef}
-      className={`lucide lucide-repeat-2 cursor-pointer hover:stroke-green-600 w-5 h-5 ${
-        isReposted ? "stroke-green-600" : "fill-none"
+      className={`lucide lucide-repeat-2 cursor-pointer hover:stroke-successful w-5 h-5 ${
+        isReposted ? 'stroke-successful' : 'fill-none stroke-foreground'
       }`}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="#000000"
       strokeWidth="2"
       stroke-linecap="round"
       stroke-linejoin="round"
